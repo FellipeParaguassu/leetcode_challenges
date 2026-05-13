792e15b25f7976e3e6ba2763f2d5b653bc8ff7d2
FellipeParaguassu <8rrfellipeparaguassu@gmail.com>
2026-05-13 17:04:55 -0400

    feat: adiciona desafios leetcode

---
+++ Python archives/sum_two.py
@@ -0,0 +1,25 @@
+'''Exercise from leetcode, level: Easy
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ 
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+'''
+class Solution:
+    def twoSum(self, nums, target):
+        seen = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in seen:
+                return [seen[complement], i]
+
+            seen[num] = i
